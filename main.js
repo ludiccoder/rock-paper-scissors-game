d120cb3e4e5228dcb1101dbdcbf242b3853cbfba
ludiccoder <algamer14@gmail.com>
2022-07-30 15:42:15 -1000

    stopping point

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+/*
 function getComputerChoice(){
     const RPS =["Rock","Paper","Scissors"];
     return computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
   }
 
 console.log(getComputerChoice())
+*/
+
+function oneRound(){
+  const playerSection = prompt("Rock, Paper, Scissors?",);
+  
+  const computerSelection = getComputerChoice()
+
+  console.log(prompt)
+}
+
 
