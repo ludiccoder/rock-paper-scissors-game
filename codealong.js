7b1155e8838bad6b3f25fb03c800ac1a3370d6ed
ludiccoder <algamer14@gmail.com>
2022-08-12 08:27:26 -1000

    game logic and troubleshooting

---
+++ codealong.js
@@ -0,0 +1,16 @@
+let admin = "John";
+let name;
+let planetName = "Earth";
+let currentVisitor;
+let x = 100 + 50;
+
+name = admin;
+
+const string = 'The revolution will not be televised.';
+
+console.log(string);
+
+const sgl = 'Single quotes.';
+const dbl = "Double quotes.";
+console.log(sgl);
+console.log(dbl);
--- main.js
+++ main.js
@@ -1,18 +1,58 @@
-/*
+const choice1 = "rock"
+const choice2 = "paper"
+const choice3 = "scissors"
+const playerSelection = prompt("Rock, Paper, Scissors?",);
+const computerSelection = getComputerChoice()
+
+return playerSelection === computerSelection
+
 function getComputerChoice(){
     const RPS =["Rock","Paper","Scissors"];
     return computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
   }
 
-console.log(getComputerChoice())
-*/
+function gameLogic(playerSelection, computerSelection){
 
-function oneRound(){
-  const playerSection = prompt("Rock, Paper, Scissors?",);
-  
-  const computerSelection = getComputerChoice()
-
-  console.log(prompt)
+if (playerSelection === computerSelection.toLowerCase){
+  document.write("It's a tie!")
+}
+else if ("rock" === "paper"){ 
+  document.write("Paper covers rock!")
+  document.write("Computer wins!")
+}
+else if ("paper" === "rock"){ 
+  document.write("Paper covers rock!")
+  document.write("Player wins!")
+}
+else if ("rock" === "scissors"){
+  document.write("Rock smashes scissors!")
+  document.write("Player wins!")
+}
+else if ("scissors" === "rock"){
+  document.write("Rock smashes scissors!")
+  document.write("Computer wins!")
+}
+else if ("scissors" === "paper"){
+  document.write("Scissors cuts paper!")
+  document.write("Player wins!") 
+}
+else if ("paper" === "scissors"){
+  document.write("Scissors cuts paper!")
+  document.write("Computer wins!")
+}
+else if ("rock" === "scissors"){
+  document.write("Rock smashes scissors!")
+  document.write("Player wins!")
+}
+else if ("scissors" === "rock"){
+  document.write("Rock smashes scissors!")
+  document.write("Computer wins!")
 }
 
+ else{
+document.write("thanks for playing!")
+}
 
+}
+
+console.log()
